1389cdbcde89266ad61f9538cb13fcab6b73a1b7
Huerta <jmhuertany@gmail.com>
2023-08-29 17:27:49 -0400

    v14.macro

--- v14.macro.py
+++ v14.macro.py
@@ -2,11 +2,12 @@ import os
 import openai
 import sys
 import yaml
+import datetime
 from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
-
+from langchain.prompts import PromptTemplate
 
 
 
@@ -22,10 +23,8 @@ sys.path.append('../..')
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
 
-
 openai.api_key  = config['KEYS']['OPENAI_API_KEY']
 
-import datetime
 current_date = datetime.datetime.now().date()
 if current_date < datetime.date(2023, 9, 2):
     llm_name = "gpt-3.5-turbo-0301"
@@ -33,17 +32,14 @@ else:
     llm_name = "gpt-3.5-turbo"
 print(llm_name)
 
-
 persist_directory = 'docs/chroma/'
 embedding = OpenAIEmbeddings(openai_api_key=openai.api_key)
 vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
-print(vectordb._collection.count())
+print("Accessed Chroma database with ",vectordb._collection.count()," documents")
 
 # Just to check all is well:
 #question = "What are major topics for this class?"
 
-
-question = "Who sets the interest rates? and What happens when interest rates rise?"
 
 #docs = vectordb.similarity_search(question,k=3)
 #len(docs)
@@ -52,13 +48,20 @@ question = "Who sets the interest rates? and What happens when interest rates ri
 ### RETREIVAL QA CHAIN & ALL DOCUMENTS ARE STUFFED INTO THE CONTEXT
 
 
-
 llm = ChatOpenAI(model_name=llm_name, temperature=0,openai_api_key=openai.api_key)
 qa_chain = RetrievalQA.from_chain_type(
     llm,
     retriever=vectordb.as_retriever(),
     chain_type="refine"
 )
+
+##  SET THE PROMPT:
+question = "What is money and who invented it?"
+question = "Who sets the interest rates? and What happens when interest rates rise?"
+
+
+print("\n\n_________________QA CHAIN with REFINE_________________________")
+
 result = qa_chain({"query":  question})
 print("RERIEVAL QA Query:",question)
 print("RETRIEVAL QA result:", result["result"])
@@ -67,9 +70,7 @@ print("RETRIEVAL QA result:", result["result"])
 
 ### PROMPT CHAIN
 
-print("_______________________________________________________")
-
-from langchain.prompts import PromptTemplate
+print("\n\n_______________ QA CHAIN with WITH PROMPT TEMPLATE____________________")
 
 # Build prompt
 
@@ -86,24 +87,20 @@ qa_chain = RetrievalQA.from_chain_type(
     return_source_documents=True,
     chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
 )
-question = "What is money and who invented it?"
+
+
 result = qa_chain({"query": question})
-
-
 print(result["result"])
 
-
+print("\n REFERENCES:")
 print("result documents")
-
-
 print(result["source_documents"][0])
 
-
+print("\n\n_________________QA CHAIN with MAP REDUCEE_________________________")
 
 
 ## REtrieval QA chain types
 
-print("Retrieval QA from chain_type  & MAP REDUCE")
 
 qa_chain_mr = RetrievalQA.from_chain_type(
     llm,
@@ -111,10 +108,10 @@ qa_chain_mr = RetrievalQA.from_chain_type(
     chain_type="map_reduce"
 )
 result = qa_chain_mr({"query": question})
-
-
-
 print(result["result"])
+
+
+
 
 '''
 #If you wish to experiment on the LangChain plus platform:
