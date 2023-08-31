41cf9ce305f9f54f324133cf0eec2bc441a7c945
Huerta <jmhuertany@gmail.com>
2023-08-31 12:07:47 -0400

    Cleaned up

--- v05.chat.py
+++ v05.chat.py
@@ -2,28 +2,21 @@ import os
 import openai
 import sys
 import yaml
-import panel as pn  # GUI
-
+import datetime
 sys.path.append('../..')
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain.memory import ConversationBufferMemory
 
-pn.extension()
-
-from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv()) # read local .env file
-
-
-
-
-
-
+# LOAD config parameters
 with open('config.yaml', 'r') as file:
     config = yaml.safe_load(file)
-
 openai.api_key  = config['KEYS']['OPENAI_API_KEY']
 
-
-
-import datetime
+# Load the LLM
 current_date = datetime.datetime.now().date()
 if current_date < datetime.date(2023, 9, 2):
     llm_name = "gpt-3.5-turbo-0301"
@@ -31,33 +24,23 @@ else:
     llm_name = "gpt-3.5-turbo"
 print(llm_name)
 
-
-
-from langchain.vectorstores import Chroma
-from langchain.embeddings.openai import OpenAIEmbeddings
+# Set the Chroma db directory
 persist_directory = 'docs/chroma/'
 
 embedding = OpenAIEmbeddings(openai_api_key=openai.api_key)
 vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
-
-question = "What are major topics for this class?"
-docs = vectordb.similarity_search(question,k=3)
-print("Len docs",len(docs))
-
-from langchain.chat_models import ChatOpenAI
 llm = ChatOpenAI(model_name=llm_name,openai_api_key=openai.api_key,  temperature=0)
-llm.predict("Hello world!")
-'Hello there! How can I assist you today?'
+
+'''
 # Build prompt
-from langchain.prompts import PromptTemplate
 template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer. 
 {context}
 Question: {question}
 Helpful Answer:"""
+
 QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"],template=template,)
 
 # Run chain
-from langchain.chains import RetrievalQA
 question = "Is probability a class topic?"
 qa_chain = RetrievalQA.from_chain_type(llm,
                                        retriever=vectordb.as_retriever(),
@@ -65,25 +48,18 @@ qa_chain = RetrievalQA.from_chain_type(llm,
                                        chain_type_kwargs={"prompt": QA_CHAIN_PROMPT})
 
 
+
+
 result = qa_chain({"query": question})
 print(result["result"])
 
 
-
-print("IM HERE A")
-
-
-
-
-
-from langchain.memory import ConversationBufferMemory
 memory = ConversationBufferMemory(
     memory_key="chat_history",
     return_messages=True
 )
 
 #ConversationalRetrievalChain
-
 from langchain.chains import ConversationalRetrievalChain
 retriever=vectordb.as_retriever()
 qa = ConversationalRetrievalChain.from_llm(
@@ -91,28 +67,8 @@ qa = ConversationalRetrievalChain.from_llm(
     retriever=retriever,
     memory=memory
 )
-question = "Is probability a class topic?"
-result = qa({"question": question})
-
-
-
-
-question = "why are those prerequesites needed?"
-result = qa({"question": question})
-print("Q:",question)
-print("A:",result['answer'])
-
-
-###
-
-print("IM HERE B")
-
-
-
-
-
-
 ######
+'''
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
@@ -122,10 +78,6 @@ from langchain.chains import RetrievalQA,  ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import TextLoader
-from langchain.document_loaders import PyPDFLoader
-
-
-
 
 
 
@@ -151,7 +103,6 @@ def load_db(chain_type, k):
 
 
 
-import panel as pn
 import param
 
 class cbfs(param.Parameterized):
@@ -167,17 +118,6 @@ class cbfs(param.Parameterized):
         self.qa = load_db("stuff", 4)
 
     
-    def call_load_db(self, count):
-        if count == 0 or file_input.value is None:  # init or no file specified :
-            return pn.pane.Markdown(f"Loaded File: {self.loaded_file}")
-        else:
-            file_input.save("temp.pdf")  # local copy
-            button_load.button_style="outline"
-            self.qa = load_db("stuff", 4)
-            button_load.button_style="solid"
-        self.clr_history()
-        return pn.pane.Markdown(f"Loaded File: {self.loaded_file}")
-
     def convchain(self, query):
         result = self.qa({"question": query, "chat_history": self.chat_history})
         self.chat_history.extend([(query, result["answer"])])
@@ -186,37 +126,9 @@ class cbfs(param.Parameterized):
         self.answer = result['answer'] 
         print('User:',query)
         print('Chatbot:',self.answer)
-        return pn.WidgetBox(*self.panels,scroll=True)
+        print('References:',self.db_response)
 
-    @param.depends('db_query ', )
-    def get_lquest(self):
-        if not self.db_query :
-            return pn.Column(
-                pn.Row(pn.pane.Markdown(f"Last question to DB:", styles={'background-color': '#F6F6F6'})),
-                pn.Row(pn.pane.Str("no DB accesses so far"))
-            )
-        return pn.Column(
-            pn.Row(pn.pane.Markdown(f"DB query:", styles={'background-color': '#F6F6F6'})),
-            pn.pane.Str(self.db_query )
-        )
-
-    @param.depends('db_response', )
-    def get_sources(self):
-        if not self.db_response:
-            return 
-        rlist=[pn.Row(pn.pane.Markdown(f"Result of DB lookup:", styles={'background-color': '#F6F6F6'}))]
-        for doc in self.db_response:
-            rlist.append(pn.Row(pn.pane.Str(doc)))
-        return pn.WidgetBox(*rlist, width=600, scroll=True)
-
-    @param.depends('convchain', 'clr_history')
-    def get_chats(self):
-        if not self.chat_history:
-            return pn.WidgetBox(pn.Row(pn.pane.Str("No History Yet")), width=600, scroll=True)
-        rlist=[pn.Row(pn.pane.Markdown(f"Current Chat History variable", styles={'background-color': '#F6F6F6'}))]
-        for exchange in self.chat_history:
-            rlist.append(pn.Row(pn.pane.Str(exchange)))
-        return pn.WidgetBox(*rlist, width=600, scroll=True)
+        
 
     def clr_history(self,count=0):
         self.chat_history = []
