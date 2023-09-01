9a6ce0ced4c4aad65c864ffef4c4959fd441bc6e
Huerta <jmhuertany@gmail.com>
2023-09-01 18:59:50 -0400

    Makinc v05c to make local capabiliteis

--- README.md
+++ README.md
@@ -34,6 +34,8 @@ https://www.trychroma.com/
 
 
 (env2) jmhuerta@b0be8379a8b7 ChatWithYourData % pip3  list
+
+
 Package             Version
 ------------------- ---------
 aiohttp             3.8.5
@@ -43,25 +45,37 @@ async-timeout       4.0.3
 attrs               23.1.0
 backoff             2.2.1
 bcrypt              4.0.1
+bleach              6.0.0
+bokeh               3.2.2
 certifi             2023.7.22
 charset-normalizer  3.2.0
 chroma-hnswlib      0.7.2
 chromadb            0.4.7
 click               8.1.7
 coloredlogs         15.0.1
+contourpy           1.1.0
 dataclasses-json    0.5.14
+faiss-cpu           1.7.4
 fastapi             0.99.1
 flatbuffers         23.5.26
 frozenlist          1.4.0
+gpt4all             1.0.9
 h11                 0.14.0
 httptools           0.6.0
 humanfriendly       10.0
 idna                3.4
 importlib-resources 6.0.1
+Jinja2              3.1.2
 langchain           0.0.273
 langsmith           0.0.26
 lark                1.1.7
+linkify-it-py       2.0.2
+Markdown            3.4.4
+markdown-it-py      3.0.0
+MarkupSafe          2.1.3
 marshmallow         3.20.1
+mdit-py-plugins     0.4.0
+mdurl               0.1.2
 monotonic           1.6
 mpmath              1.3.0
 multidict           6.0.4
@@ -72,6 +86,10 @@ onnxruntime         1.15.1
 openai              0.27.9
 overrides           7.4.0
 packaging           23.1
+pandas              2.0.3
+panel               1.2.1
+param               1.13.0
+Pillow              10.0.0
 pip                 23.2.1
 posthog             3.0.2
 protobuf            4.24.1
@@ -81,6 +99,8 @@ pypdf               3.15.2
 PyPika              0.48.9
 python-dateutil     2.8.2
 python-dotenv       1.0.0
+pytz                2023.3
+pyviz_comms         3.0.0
 PyYAML              6.0.1
 regex               2023.8.8
 requests            2.31.0
@@ -93,12 +113,17 @@ sympy               1.12
 tenacity            8.2.3
 tiktoken            0.4.0
 tokenizers          0.13.3
+tornado             6.3.3
 tqdm                4.66.1
 typing_extensions   4.7.1
 typing-inspect      0.9.0
+tzdata              2023.3
+uc-micro-py         1.0.2
 urllib3             2.0.4
 uvicorn             0.23.2
 uvloop              0.17.0
 watchfiles          0.20.0
+webencodings        0.5.1
 websockets          11.0.3
+xyzservices         2023.7.0
 yarl                1.9.2
--- v05c.chat_local.py
+++ v05c.chat_local.py
@@ -22,19 +22,41 @@ from langchain.chains import RetrievalQA,  ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import TextLoader
+from langchain.embeddings import GPT4AllEmbeddings
+from langchain.llms import GPT4All
+#from gpt4all import GPT4All
 
 
+## ToDo:
+## 1. LocalEmbeddings
+###        https://docs.gpt4all.io/index.html
+###        https://python.langchain.com/docs/use_cases/question_answering/how_to/local_retrieval_qa
+## 2. LocalLLm
+##   https://python.langchain.com/docs/use_cases/question_answering/how_to/local_retrieval_qa
+#    https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/tree/main
 
-# LOAD config parameters
-with open('config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
-openai.api_key  = config['KEYS']['OPENAI_API_KEY']
+local = True
+
+if local is True:
+    model = GPT4All(model="orca-mini-3b.ggmlv3.q4_0.bin",max_tokens=2048)
+else:
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+    openai.api_key  = config['KEYS']['OPENAI_API_KEY']
+    current_date = datetime.datetime.now().date()
+    if current_date < datetime.date(2023, 9, 2):
+        llm_name = "gpt-3.5-turbo-0301"
+    else:
+        llm_name = "gpt-3.5-turbo"
+    print("LLM Name: ", llm_name)
+    print(llm_name)
+    model = ChatOpenAI(model_name=llm_name,openai_api_key=openai.api_key,  temperature=0)
 
 
 
 def create_on_the_fly_KB():
     loaders = [
-        # Duplicate documents on purpose - messy data                                                                                                                                                                                    
+        # Duplicate documents on purpose - messy data                                                                                                       
         PyPDFLoader("docs/macro/Livro Macro.pdf"),
         PyPDFLoader("docs/macro/Macroeconomics_IntroReview.pdf"),
         PyPDFLoader("docs/macro/n.-gregory-mankiw-macroeconomics-7th-edition-2009.pdf")
@@ -43,7 +65,7 @@ def create_on_the_fly_KB():
     for loader in loaders:
         docs.extend(loader.load())
             
-    # Define the Text Splitter                                                                                                                                                                                                                   
+    # Define the Text Splitter
     text_splitter = RecursiveCharacterTextSplitter(
         #chunk_size = 1500,                                                                                                                                                                                                                  
         chunk_size = 1500,
@@ -51,12 +73,11 @@ def create_on_the_fly_KB():
         separators=["\n\n","\n","(?<=\. )"," ",""]
     )
 
-
     chunks = text_splitter.split_documents(docs)
     #chunks = text_splitter.split_text(docs)
     # Convert the chunks of text into embeddings to form a knowledge base
-    embeddings = OpenAIEmbeddings(openai_api_key=openai.api_key)
-
+    #embeddings = OpenAIEmbeddings(openai_api_key=openai.api_key)
+    embeddings=GPT4AllEmbeddings()
     knowledgeBase = FAISS.from_documents(chunks, embeddings)    
     return knowledgeBase
 
@@ -64,14 +85,6 @@ def create_on_the_fly_KB():
 faiss_kb = create_on_the_fly_KB()
 
 
-current_date = datetime.datetime.now().date()
-if current_date < datetime.date(2023, 9, 2):
-    llm_name = "gpt-3.5-turbo-0301"
-else:
-    llm_name = "gpt-3.5-turbo"
-print(llm_name)
-
-llm = ChatOpenAI(model_name=llm_name,openai_api_key=openai.api_key,  temperature=0)
 
 
 
@@ -79,7 +92,7 @@ llm = ChatOpenAI(model_name=llm_name,openai_api_key=openai.api_key,  temperature
 def load_db(chain_type, k):
     # load documents
     # define embedding
-    embeddings = OpenAIEmbeddings(openai_api_key=openai.api_key)
+    #embeddings = OpenAIEmbeddings(openai_api_key=openai.api_key)
     # create vector database from data
     #db = DocArrayInMemorySearch.from_documents(docs, embeddings)
     # define retriever
@@ -88,7 +101,8 @@ def load_db(chain_type, k):
     retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": k})
     # create a chatbot chain. Memory is managed externally.
     qa = ConversationalRetrievalChain.from_llm(
-        llm = ChatOpenAI(model_name=llm_name,openai_api_key=openai.api_key,  temperature=0),
+        #llm = ChatOpenAI(model_name=llm_name,openai_api_key=openai.api_key,  temperature=0),
+        llm=model,
         chain_type=chain_type, 
         retriever=retriever, 
         return_source_documents=True,
